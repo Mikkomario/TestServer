7045ba795acdd0a9787a3e95b4faff2819555466
Mikkomario <mikko_hilpinen@hotmail.com>
2018-05-13 12:52:33 +0300

    Added a new echo servlet

---
+++ WEB-INF/src/servlet/EchoServlet.scala
@@ -0,0 +1,68 @@
+package servlet
+
+import javax.servlet.http.HttpServlet
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+import utopia.flow.generic.ValueConversions._
+import java.time.Instant
+import http.Response
+import utopia.flow.generic.DataType
+import utopia.flow.generic.StringType
+import collection.JavaConverters._
+import utopia.flow.util.NullSafe._
+import scala.util.Try
+import scala.util.Success
+import scala.util.Failure
+import javax.servlet.annotation.MultipartConfig
+import servlet.HttpExtensions._
+import http.ServerSettings
+import java.nio.file.Paths
+import utopia.flow.datastructure.mutable
+import utopia.flow.datastructure.immutable.Model
+import http.Body
+
+/**
+ * This servlet implementation responds with data collected from the request
+ * @author Mikko Hilpinen
+ * @since 21.8.2017
+ */
+@MultipartConfig(
+        fileSizeThreshold   = 1048576,  // 1 MB
+        maxFileSize         = 10485760, // 10 MB
+        maxRequestSize      = 20971520, // 20 MB
+        location            = "D:/Uploads"
+)
+class EchoServlet extends HttpServlet
+{
+    // INITIAL CODE    -----------------------
+    
+    DataType.setup()
+    private implicit val settings = ServerSettings("http://localhost:9999", Paths.get("D:/Uploads"))
+    
+    
+    // IMPLEMENTED METHODS    ----------------
+    
+    override def doGet(req: HttpServletRequest, res: HttpServletResponse) = 
+    {
+        val request = req.toRequest.get
+        
+        val buffer = new mutable.Model()
+        buffer.update("method", request.method.toString())
+        buffer.update("url", request.targetUrl)
+        buffer.update("path", request.path.map(_.toString()))
+        buffer.update("parameters", request.parameters)
+        buffer.update("headers", Model.fromMap(request.headers.fields))
+        buffer.update("parts", request.body.toVector.map(partToModel))
+        
+        Response.fromModel(buffer.immutableCopy()).update(res)
+    }
+    
+    override def doPost(request: HttpServletRequest, response: HttpServletResponse) = doGet(request, response)
+    override def doPut(request: HttpServletRequest, response: HttpServletResponse) = doGet(request, response)
+    override def doDelete(request: HttpServletRequest, response: HttpServletResponse) = doGet(request, response)
+    override def doHead(request: HttpServletRequest, response: HttpServletResponse) = doGet(request, response)
+    
+    private def partToModel(part: Body) = Model(Vector("name" -> part.name, 
+            "size" -> part.contentLength, "type" -> part.contentType.toString, 
+            "headers" -> Model.fromMap(part.headers.fields)))
+}
